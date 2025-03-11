7eb9131eeee3cddf34d2a4e6e51c04b782b3fc83
Eren Mustafa Ceylan <eren-mustafa.ceylan@insa-lyon.fr>
2025-03-11 19:43:07 +0100

    Postgre connection string config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,15 +7,10 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      // ssl: {
-      //   rejectUnauthorized: false,
-      // },
-      ssl: false,
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
     debug: false,
   },
